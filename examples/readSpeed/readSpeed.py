7cc0d6083cf92f031c243b262297b75a3cf6894d
Jonathan Piat <piat.jonathan@gmail.com>
2020-01-28 08:00:17 -0800

    Modified example

--- examples/readSpeed/readSpeed.py
+++ examples/readSpeed/readSpeed.py
@@ -8,9 +8,11 @@ SENS_ELECTRICAL = 0x08
 
 ctrl = hoverboard_api.HoverboardAPI(b"/dev/ttyTHS1")
 
+#ctrl.sendSpeedData(2.0, 0.0, 500, 0)
 while True:
+	#ctrl.sendSpeedData(1.0, 1.0, 500, 0)
 	ctrl.sendPWM(100, 0)
 	ctrl.requestRead(SENS_HALL);
 	ctrl.protocolTask()
 	time.sleep(0.03)
-	print("speed {}".format(ctrl.getSpeed_kmh()))
+	print("speed {} {}".format(ctrl.getSpeed0_kmh(), ctrl.getSpeed1_kmh()))
